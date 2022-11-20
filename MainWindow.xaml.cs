a31fab60b3e47e3d942bf48badec7982055cb6a7
maxit32 <kornilev.maxim@mail.ru>
2022-11-20 17:55:43 +0300

    v 0.2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,7 +20,7 @@ using System.Windows.Shapes;
 
 namespace keyboard_prj
 {
-   
+
 
 
     public partial class MainWindow : Window
@@ -38,12 +38,18 @@ namespace keyboard_prj
             //настройка таймера
             timer1 = new System.Windows.Threading.DispatcherTimer();
             timer1.Tick += new EventHandler(timer_Tick);
+
+            //привязка slider и difficulty
+            Binding b = new Binding();
+            b.ElementName = "slider_d";
+            b.Path = new PropertyPath("Value"); // свойство элемента-источника
+            label_d.SetBinding(Label.ContentProperty, b);
         }
 
         #region [Skins]
         private void Skin1_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void Skin2_Click_1(object sender, RoutedEventArgs e)
@@ -65,9 +71,9 @@ namespace keyboard_prj
 
         private void End_b_MouseEnter_1(object sender, MouseEventArgs e)
         {
-            Animation_button(sender,e);
+            Animation_button(sender, e);
         }
-        
+
         private void Animation_button(object sender, MouseEventArgs e)
         {
             var anime = new DoubleAnimation();
@@ -76,34 +82,61 @@ namespace keyboard_prj
             anime.Duration = TimeSpan.FromMilliseconds(2000);
             (sender as Button).BeginAnimation(OpacityProperty, anime);
         }
+
+        private void mi_0_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as MenuItem).Background = Brushes.Yellow;
+        }
+
+        private void mi_1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as MenuItem).Background = Brushes.Red;
+        }
+
+        private void mi_2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as MenuItem).Background = Brushes.Green;
+        }
         #endregion
 
         #region [Timer]
         private void Start_b_Click(object sender, RoutedEventArgs e)
         {
+            string value;
+            bool IsNormal = false;
             //отключим старый
-            if(timer1.IsEnabled)
+            if (timer1.IsEnabled)
             {
                 timer1.Stop();
             }
+            //в строку
+            value = slider_d.Value.ToString();
+            //проверка на целое число
+            if(value.Length > 2)
+            {
+                IsNormal = false;
+            }
+            else { IsNormal= true; }
+
             //включим новый
-            if(slider_d.Value > 0)
+            if (slider_d.Value > 0 && IsNormal)
             {
                 timer1.Interval = new TimeSpan(0, 0, (int)slider_d.Value);
                 timer1.Start();
             }
             else
             {
-                MessageBox.Show("Установите сложность слайдером!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }    
+                MessageBox.Show($"Установите сложность слайдером!{Environment.NewLine}Внимание! Значение должно быть целочисленным!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
 
         private void End_b_Click(object sender, RoutedEventArgs e)
         {
             //выключаем
-            if(timer1.IsEnabled)
+            if (timer1.IsEnabled)
             {
                 timer1.Stop();
+                MessageBox.Show("Тамер выключен.", "", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
 
